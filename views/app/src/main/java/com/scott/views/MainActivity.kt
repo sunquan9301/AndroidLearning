6cb21a905d133d43da5ad35cd3be68e9008f7c58
sunquan <sunquan9301@163.com>
2020-07-24 16:04:19 +0800

    [Feature]update views

--- views/app/src/main/java/com/scott/views/MainActivity.kt
+++ views/app/src/main/java/com/scott/views/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.scott.views
 
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import java.io.File
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_cardview)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            val files: Array<File> = getExternalFilesDirs(Environment.MEDIA_MOUNTED)
+            for (file in files) {
+                Log.e("file_dir", file.getAbsolutePath())
+            }
+        }
+
     }
 }
