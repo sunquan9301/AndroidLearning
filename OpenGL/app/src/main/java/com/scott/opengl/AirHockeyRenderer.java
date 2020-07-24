a789b3919e6e85b18b13a7f46ce1b3e364693aab
sunquan <sunquan9301@163.com>
2020-07-24 17:12:35 +0800

    [Feature]add construct simple object

--- OpenGL/app/src/main/java/com/scott/opengl/AirHockeyRenderer.java
+++ OpenGL/app/src/main/java/com/scott/opengl/AirHockeyRenderer.java
@@ -6,6 +6,7 @@ import android.opengl.Matrix;
 import android.util.Log;
 
 import com.scott.opengl.objects.Mallet;
+import com.scott.opengl.objects.Puck;
 import com.scott.opengl.objects.Table;
 import com.scott.opengl.objects.TriAngle;
 import com.scott.opengl.programs.ColorShaderProgram;
@@ -16,16 +17,24 @@ import com.scott.opengl.util.TextureHelper;
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
+import static android.opengl.GLES20.GL_COLOR_BUFFER_BIT;
+import static android.opengl.GLES20.glClear;
 import static android.opengl.GLES20.glViewport;
+import static android.opengl.Matrix.setLookAtM;
 
 public class AirHockeyRenderer implements GLSurfaceView.Renderer {
     private final Context context;
 
     private final float[] projectionMatrix = new float[16];
     private final float[] modelMatrix = new float[16];
+    private final float[] viewMatrix = new float[16];
+    private final float[] viewProjectionMatrix = new float[16];
+    private final float[] modelViewProjectionMatrix = new float[16];
+
 
     private Table table;
     private Mallet mallet;
+    private Puck puck;
     private TriAngle triAngle;
 
     private TextureShaderProgram textureProgram;
@@ -42,8 +51,10 @@ public class AirHockeyRenderer implements GLSurfaceView.Renderer {
     public void onSurfaceCreated(GL10 gl, EGLConfig config) {
         Log.i("sun_opengl", "onSurfaceCreated");
         table = new Table();
-        mallet = new Mallet();
-        triAngle = new TriAngle();
+        mallet = new Mallet(0.08f, 0.15f, 32);
+        puck = new Puck(0.06f, 0.02f, 32);
+
+//        triAngle = new TriAngle();
 
         textureProgram = new TextureShaderProgram(context);
         colorProgram = new ColorShaderProgram(context);
@@ -55,33 +66,73 @@ public class AirHockeyRenderer implements GLSurfaceView.Renderer {
     public void onSurfaceChanged(GL10 gl, int width, int height) {
         Log.i("sun_opengl", "onSurfaceChanged,w = " + width + ",height = " + height);
         glViewport(0, 0, width, height);
+        MatrixHelper.perspectiveM(projectionMatrix, 45, (float) width
+                / (float) height, 1f, 10f);
+        setLookAtM(viewMatrix, 0, 0f, 1.2f, 2.2f, 0f, 0f, 0f, 0f, 1f, 0f);
 
-        MatrixHelper.perspectiveM(projectionMatrix, 120, (float) width
-                / (float) height, 0f, 10f);
-
-        Matrix.setIdentityM(modelMatrix, 0);
-
-        Matrix.translateM(modelMatrix, 0, 0f, 0f, -1f);
-//        Matrix.rotateM(modelMatrix, 0, -40f, 1f, 0f, 0f);
-
-        final float[] temp = new float[16];
-        Matrix.multiplyMM(temp, 0, projectionMatrix, 0, modelMatrix, 0);
-        System.arraycopy(temp, 0, projectionMatrix, 0, temp.length);
+//        MatrixHelper.perspectiveM(projectionMatrix, 120, (float) width
+//                / (float) height, 0f, 10f);
+//
+//        Matrix.setIdentityM(modelMatrix, 0);
+//
+//        Matrix.translateM(modelMatrix, 0, 0f, 0f, -1f);
+////        Matrix.rotateM(modelMatrix, 0, -40f, 1f, 0f, 0f);
+//
+//        final float[] temp = new float[16];
+//        Matrix.multiplyMM(temp, 0, projectionMatrix, 0, modelMatrix, 0);
+//        System.arraycopy(temp, 0, projectionMatrix, 0, temp.length);
     }
 
     @Override
     public void onDrawFrame(GL10 gl) {
 
-//         Draw the table.
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Multiply the view and projection matrices together.
+        Matrix.multiplyMM(viewProjectionMatrix, 0, projectionMatrix, 0, viewMatrix, 0);
+
+        // Draw the table.
+        positionTableInScene();
         textureProgram.useProgram();
-        textureProgram.setUniforms(projectionMatrix, texture);
+        textureProgram.setUniforms(modelViewProjectionMatrix, texture);
         table.bindData(textureProgram);
         table.draw();
 
         // Draw the mallets.
+        positionObjectInScene(0f, mallet.height / 2f, -0.4f);
         colorProgram.useProgram();
-        colorProgram.setUniforms(projectionMatrix);
+        colorProgram.setUniforms(modelViewProjectionMatrix, 1f, 0f, 0f);
         mallet.bindData(colorProgram);
         mallet.draw();
+
+        positionObjectInScene(0f, mallet.height / 2f, 0.4f);
+        colorProgram.setUniforms(modelViewProjectionMatrix, 0f, 0f, 1f);
+        // Note that we don't have to define the object data twice -- we just
+        // draw the same mallet again but in a different position and with a
+        // different color.
+        mallet.draw();
+
+        // Draw the puck.
+        positionObjectInScene(0f, puck.height / 2f, 0f);
+        colorProgram.setUniforms(modelViewProjectionMatrix, 0.8f, 0.8f, 1f);
+        puck.bindData(colorProgram);
+        puck.draw();
+    }
+
+    private void positionTableInScene() {
+        // The table is defined in terms of X & Y coordinates, so we rotate it
+        // 90 degrees to lie flat on the XZ plane.
+        Matrix.setIdentityM(modelMatrix, 0);
+        Matrix.rotateM(modelMatrix, 0, -90f, 1f, 0f, 0f);
+        Matrix.multiplyMM(modelViewProjectionMatrix, 0, viewProjectionMatrix,
+                0, modelMatrix, 0);
+    }
+
+    // The mallets and the puck are positioned on the same plane as the table.
+    private void positionObjectInScene(float x, float y, float z) {
+        Matrix.setIdentityM(modelMatrix, 0);
+        Matrix.translateM(modelMatrix, 0, x, y, z);
+        Matrix.multiplyMM(modelViewProjectionMatrix, 0, viewProjectionMatrix,
+                0, modelMatrix, 0);
     }
 }
--- OpenGL/app/src/main/java/com/scott/opengl/objects/Puck.java
+++ OpenGL/app/src/main/java/com/scott/opengl/objects/Puck.java
@@ -14,26 +14,24 @@ import com.scott.opengl.util.Geometry;
 
 import java.util.List;
 
-
-public class Mallet {
+public class Puck {
     private static final int POSITION_COMPONENT_COUNT = 3;
 
-    public final float radius;
-    public final float height;
+    public final float radius, height;
 
     private final VertexArray vertexArray;
     private final List<ObjectBuilder.DrawCommand> drawList;
 
-    public Mallet(float radius, float height, int numPointsAroundMallet) {
-        ObjectBuilder.GeneratedData generatedData = ObjectBuilder.createMallet(new Geometry.Point(0f,
-            0f, 0f), radius, height, numPointsAroundMallet);
-
+    public Puck(float radius, float height, int numPointsAroundPuck) {
+        ObjectBuilder.GeneratedData generatedData = ObjectBuilder.createPuck(new Geometry.Cylinder(
+            new Geometry.Point(0f, 0f, 0f), radius, height), numPointsAroundPuck);
         this.radius = radius;
         this.height = height;
 
         vertexArray = new VertexArray(generatedData.vertexData);
         drawList = generatedData.drawList;
     }
+
     public void bindData(ColorShaderProgram colorProgram) {
         vertexArray.setVertexAttribPointer(0,
             colorProgram.getPositionAttributeLocation(),
